f3d2a2139eea47c4cd3495c0a27bac8528b30ec3
Pranav Shah <pranavshah01@gmail.com>
2026-06-27 00:24:42 -0700

    Fix main.tsx: use correct getRouter function

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./router";
+import { getRouter } from "./router";
+
+const router = getRouter();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
